c2776faf51365794f8d2b4e6590bd2c92f690116
Simon Pallister <sion.pallister@bigcommerce.com>
2021-09-06 08:07:32 +1000

    update readme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
+import { GlobalStyles } from '@bigcommerce/big-design'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600&display=swap" rel="stylesheet" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
